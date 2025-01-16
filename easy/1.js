caa482be2d065ae4bf55f6779686d30a0eeeaaa8
jamesbatayola <batayolajakejames@gmail.com>
2025-01-17 07:27:39 +0800

    easy_03

--- easy/1.js
+++ easy/1.js
@@ -22,16 +22,33 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-function twoSum(nums, target) {
+// function twoSum(nums, target) {
+//    for (let i = 0; i < nums.length; i++) {
+//       for (let j = i + 1; j < nums.length; j++) {
+//          if (nums[i] + nums[j] === target) {
+//             return [i, j];
+//          }
+//       }
+//    }
+// }
+
+// --- OPTIAMAL --- //
+
+function _twoSum(nums, target) {
+   const pairs = new Map();
+
    for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-         if (nums[i] + nums[j] === target) {
-            return [i, j];
-         }
+      pairs.set(nums[i], i);
+   }
+
+   for (const [key, value] of pairs) {
+      const numToGet = target - key;
+      if (pairs.get(numToGet) !== undefined) {
+         return [pairs.get(key), pairs.get(numToGet)];
       }
    }
+
+   return -1;
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
-console.log(twoSum([3, 3], 6));
+console.log(_twoSum([2, 7, 11, 15], 9));
